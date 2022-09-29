c53b815249905cf0c0cb64a79e832a1cea590289
rlalastjd782 <rlalastjd782@naver.com>
2022-09-29 17:52:44 +0900

    인프런스프링부트 Jpa 로 회원가입만들기

---
+++ helllo/src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,27 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.JpaMemberRepository;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
+
+@Configuration
+public class SpringConfig {
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+
+}
+
